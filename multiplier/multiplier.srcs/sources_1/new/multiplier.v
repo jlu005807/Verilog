9d8e43cd2218dbc2647fc613d4c5560fc6fc7903
jlu005807 <1289406660@qq.com>
2024-11-21 10:01:50 +0800

    finish multiplier

--- multiplier/multiplier.srcs/sources_1/new/multiplier.v
+++ multiplier/multiplier.srcs/sources_1/new/multiplier.v
@@ -33,37 +33,44 @@ reg [7:0] op1_comp;
 reg [7:0] op2_comp;
 reg [15:0] now_product;
 
-initial fork
-    if(mult_op1[7]==1'b1)
-        op1_comp = ~mult_op1 + 1'b1;
-    else
-        op1_comp = mult_op1;
-    if(mult_op2[7]==1'b1)
-        op2_comp = ~mult_op2 + 1'b1;
-    else
-        op2_comp = mult_op2;
-    now_product = 16'b0000000000000000;
-
-join
 
 
 always @(posedge button_clk or posedge reset) begin
-    if(reset) begin
+    if(reset) fork
+        if(mult_op1[7]==1'b1)
+            op1_comp = ~mult_op1 + 1'b1;
+        else
+            op1_comp = mult_op1;
+        if(mult_op2[7]==1'b1)
+            op2_comp = ~mult_op2 + 1'b1;
+        else
+            op2_comp = mult_op2;
         now_product = 16'b0000000000000000;
-        op1_comp = mult_op1;
-        op2_comp = mult_op2;
-    end
+    join
     else begin
         if (op2_comp[0] == 1'b1)
             now_product = op1_comp + now_product;
         op2_comp = op2_comp >> 1;
-        now_product = now_product << 1;
+        op2_comp[7]=now_product[0];
+        now_product = now_product >> 1;
     end
 end
 
 pointer counter(button_clk,reset,num_seg7);
 
-assign mult_product = {mult_op1[7]^mult_op2[7],now_product[11:5]};
+reg [6:0] temp;
+wire signal;
+
+assign signal = mult_op1[7]^mult_op2[7];
+
+always @(*) begin
+    if(signal==1'b1)
+        temp = ~{now_product[2:0],op2_comp[7:4]} + 1'b1;
+    else
+        temp = {now_product[2:0],op2_comp[7:4]};
+end
+
+assign mult_product = {signal, temp};
 
 endmodule
 
